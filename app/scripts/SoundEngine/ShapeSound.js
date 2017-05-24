770d28fb8ed8ffd7067b3d1573f7192242fcd4de
David Rubin <dudrubin@gmail.com>
2017-05-24 08:00:00 +0300

    Added timeline events Added play override

--- app/scripts/SoundEngine/ShapeSound.js
+++ app/scripts/SoundEngine/ShapeSound.js
@@ -4,57 +4,90 @@
  */
 var audioCtx = new AudioContext();
 class ShapeSound {
-  constructor(timeline = [], totalduration = 0) {
+
+  /**
+   * C'tor
+   * @param timeline - array of TimelineEvent objects
+   * @param totalDuration - the total duration of the sequence
+   */
+  constructor(timeline = [], totalDuration = 0) {
+    this.startingAmp = 0.4;
+    var startingCutoff = 1000;
     this.timeline = timeline;
-    this.totalduration = totalduration;
-    this.oscillator = audioCtx.createOscillator();
+    this.totalDuration = totalDuration;
     this.gainNode = audioCtx.createGain();
-    this.oscillator.connect(this.gainNode);
-    this.oscillator.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-    this.oscillator.frequency.value = 800; // value in hertz
-    this.oscillator.start();
+    this.gainNode.gain.value = this.startingAmp;
+    this.filter = audioCtx.createBiquadFilter();
+    this.filter.type = "lowshelf";
+    this.filter.frequency.value = startingCutoff;
+    this.filter.gain.value = 30;
+    this.gainNode.connect(this.filter);
+    this.filter.connect(audioCtx.destination);
+    this.playing = false;
+    this.intervals = [];
   }
 
   /**
    * Start playing the given timeline
    */
   play() {
-    this.gainNode.connect(audioCtx.destination);
+    //make sure instance isn't playing already
+    this.stopPlaying();
+
+    //create new oscilator
+    this.oscillator1 = audioCtx.createOscillator();
+    this.oscillator1.type = 'sawtooth'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+    this.oscillator1.connect(this.gainNode);
+    this.oscillator2 = audioCtx.createOscillator();
+    this.oscillator2.type = 'triangle'; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+    this.oscillator2.connect(this.gainNode);
+    this.oscillator1.detune.value = -10;
+    this.oscillator2.detune.value = 10;
+
+    //then schedule control
     for (var i = 0; i < this.timeline.length; i++) {
       var obj = this.timeline[i];
-      this.delay(obj.time, obj.message).then(event=>this.handleTimelineEvent(event));
+      var p = setTimeout(this.handleTimelineEvent.bind(this, obj), obj.time);
+      this.intervals.push(p);
     }
 
-    this.delay(this.totalduration, null).then(()=>this.stopPlaying());
+    var endingP = setTimeout(()=>this.stopPlaying(), this.totalDuration);
+    this.intervals.push(endingP);
   }
 
+  /**
+   * If oscs are playing stop them , and clear all future intervals
+   */
   stopPlaying() {
-    console.log("STOP!");
-    this.gainNode.disconnect();
-  }
 
-  delay(ms, obj) {
-    return new Promise(function (resolve, reject) {
-      setTimeout(()=>resolve(obj), ms);
-    });
+    if (this.isPlaying) {
+      this.isPlaying = false;
+      this.oscillator1.stop();
+      this.oscillator2.stop();
+    }
+
+    //clear all current events
+    for (var i = 0; i < this.intervals.length; i++) {
+      clearInterval(this.intervals[i]);
+    }
+    this.intervals = [];
   }
 
   /**
    * Gets the timeline event and alters sin according to it
-   * @param event
+   * @param event - TimelineEvent object
    */
   handleTimelineEvent(event) {
-    console.log(event);
-    this.oscillator.frequency.value = event.freq;
-    this.gainNode.gain.value= event.amp;
-    this.timeline;
+
+    if (!this.isPlaying) {
+      this.isPlaying = true;
+      this.oscillator1.start();
+      this.oscillator2.start();
+    }
+
+    this.oscillator1.frequency.value = event.freq;
+    this.oscillator2.frequency.value = event.freq;
+    this.gainNode.gain.value = event.amp * this.startingAmp;
+    this.filter.frequency.value=   event.cutoff;
   };
-
-
 }
-
-var a = new ShapeSound([
-  {time: 500, message: {amp:0.2,freq:700}},
-  {time: 1000, message: {amp:1,freq:800}},
-  {time: 1500, message: {amp:0.8,freq:100}}], 2000);
-a.play();
