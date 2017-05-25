75663d49a66c59694a23764ba3c8f28d2d4907e3
David Rubin <dudrubin@gmail.com>
2017-05-25 23:48:16 +0300

    Added kill list + fade effect Added som documentations

--- app/scripts/view/Main.js
+++ app/scripts/view/Main.js
@@ -4,22 +4,52 @@
  */
 class Main {
   constructor() {
+    //initialise paper
     paper.install(window);
     var canvas = document.getElementById('myCanvas');
     paper.setup(canvas);
 
-
+    //set vars
     this.isDrawing = false;
     this.path = null;
     this.timelineEvents = [];
+    this.killList = [];
     this.sound = new LiveSound();
 
-
+    //set events
     view.onMouseDown = (e)=>this.startDraw(e);
     view.onMouseUp = (e)=>this.stopDraw(e);
     view.onMouseMove = (e)=>this.draw(e);
+    view.onFrame = (e)=>this.onFrame(e);
+
+    //TODO - Add bar counter to set some kind of frame for all of this
   }
 
+  /**
+   * If kill list is has elements, fade them and then kill them after they fade out
+   * @param event
+   */
+  onFrame(event) {
+    if (this.killList.length > 0) {
+      var i = this.killList.length;
+      while (i--) {
+        var path = this.killList[i];
+        if (path.strokeColor.alpha > 0) {
+          path.strokeColor.alpha -= 0.05;
+        }
+        else {
+          console.log("removeing from list");
+          this.killList.splice(i, 1);
+          path.remove();
+        }
+      }
+    }
+  }
+
+  /**
+   * When user starts a draw, record its path and let live sound play that path.
+   * @param event
+   */
   startDraw(event) {
     console.log('Start draw ');
     if (this.isDrawing) return;
@@ -37,14 +67,24 @@ class Main {
     });
   }
 
+  /**
+   * When user stops from drawing shape,
+   * TODO - create the looper visual\sonic objects
+   * Insert finished path to kill list
+   * @param event
+   */
   stopDraw(event) {
     this.sound.stop();
     console.log("Stop Draw");
     this.isDrawing = false;
     this.path.simplify(10);
-    console.log(this.timelineEvents);
+    this.killList.push(this.path);
   }
 
+  /**
+   * when user moves. if drawing, then  alter the live sound according to axis\speed\vector
+   * @param event
+   */
   draw(event) {
     if (this.isDrawing) {
       var p = event.point;
@@ -53,8 +93,8 @@ class Main {
       this.path.add(p);
       this.sound.updateFrequency(p.x + 400);
       // this.sound.updateVolume(p.y/1400);
-      this.sound.updateFilter(Math.min(Math.abs(speed.x)*400,2050));
-      this.timelineEvents.push(new CanvasTimelineEvent(timePassed,p,speed));
+      this.sound.updateFilter(Math.min(Math.abs(speed.x) * 400, 2050));
+      this.timelineEvents.push(new CanvasTimelineEvent(timePassed, p, speed));
       this.lastPoint = p;
     }
   }
