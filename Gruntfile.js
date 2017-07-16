a42d8deefe1482f332dc7588044ef6ff79bb4c6c
David Rubin <dudrubin@gmail.com>
2017-07-16 22:45:48 +0300

    Added new diet folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -411,6 +411,7 @@ module.exports = function (grunt) {
           dest: '<%= yeoman.dist %>',
           src: [
             '*.{ico,png,txt}',
+            'sounds/*',
             '*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
